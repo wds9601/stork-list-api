76f878d9a53ba031e7b68ea3c638a97d62107a1b
William Downey-Schell <Will@Williams-MacBook-Air.local>
2020-09-12 01:00:25 -0700

    added some todos

---
+++ server.js
@@ -0,0 +1,8 @@
+// TODO
+//npm install express, sequelize OR knex, cors, pg, bcrypt
+// instantiate app and start server (app.listen...)
+// Stub routes
+    // see notebook
+// create db schema and createdb, Postgres
+//
+//...
